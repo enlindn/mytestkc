821a59dcb3e5fac30c57725b317e33f49a198ea8
lirilsu <Illution@Pandora.local>
2015-05-27 23:56:58 +0800

    Something wrong 2
    Here is some wrong, and will not be used.2015-5-27.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+<<<<<<< Updated upstream
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <!--    css here    -->
+        <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+=======
+/**
+ * Created by PhpStorm.
+ * User: Illution
+ * Date: 5/27/15
+ * Time: 20:36
+ */
+>>>>>>> Stashed changes
+
+header("location: login.php");
